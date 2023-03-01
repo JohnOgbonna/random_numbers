ab2bdb43cef712802b239f97ef36d3611afc49a7
JohnOgbonna <johnny.ogb@gmail.com>
2023-03-01 15:12:07 -0700

    random numbers

---
+++ 10000randomNumbersScript.js
@@ -0,0 +1,48 @@
+let form = document.getElementById('form');
+
+function generateRandomNumbers(min, max, digits, decimals){
+    let randomNumbers = []
+    let index = 0
+    while (randomNumbers.length < digits) {
+        let number = +(Math.random() * (max-min) + min).toFixed(decimals) //set range of numbers based on max and min
+        if (!randomNumbers.includes(number)) { //only add number if not inside random numbers array to avoid duplication
+            randomNumbers.push(number)
+            console.log(randomNumbers[index]) //prints out the numbers in the console
+            index++ //index tracks numbers added to random numbers
+        }
+    }
+    let numbersList = document.getElementById('randomNumbers')
+    numbersList.innerHTML = '' //clear any existing list
+    for(let i = 0; i < randomNumbers.length; i++ ){
+        numbersList.appendChild(document.createElement('li')).innerHTML = randomNumbers[i]
+    }
+    let confirmation = document.getElementById('confirmation')
+    confirmation.innerHTML = ''
+    confirmation.innerHTML = `Generated ${randomNumbers.length} Random Number${randomNumbers.length > 1 ? 's' : ''}`
+}
+
+form.addEventListener('submit', e =>{
+    e.preventDefault()
+    //series of if statements for validation
+    if (+ e.target.min.value > + e.target.max.value){
+        alert("Minimum value cannot be higher than the Maximum")
+        return
+    }
+    if (e.target.digits.value > (e.target.max.value - e.target.min.value + 1 )){
+        alert("Too many requested digits for given range") //this is important as there are no duplicates allowed. If range is not long enough, would case infinite loop
+        return
+    }
+    if (e.target.digits.value > 1000000){
+        alert("Limit for this program is 1,000,000 numbers!")
+        return
+    }
+    generateRandomNumbers(+e.target.min.value, +e.target.max.value, +e.target.digits.value, +e.target.decimals.value)
+
+})
+clearButton = document.getElementById('clear')
+clearButton.addEventListener('click', e =>{
+    e.preventDefault()
+    numbersList = document.getElementById('randomNumbers')
+    numbersList.innerHTML = '' //clear any existing list
+    confirmation.innerHTML = '' //clear confirmation message
+})
---
+++ README.md
@@ -0,0 +1,9 @@
+-Step 1:
+    Open the index.html file using a browser of your choice. Make sure the file '10000randomNumbersScript.js' is in the same directory
+-Step 2 
+    Select range and decimal places up (up to 10 decimal places) and type in the number of digits you want. These numbers default to the initial requirement of 10,000 random numbers within a range of 1 to 10,000 with 2 decimal places.
+-Step 3 
+    Select 'Generate' and the numbers will appear in a scrollable list beneath the generate button. There will be no duplicate numbers, as requested
+-Step 4
+    Hit the clear button and try again as desired!
+
